863c6223e5f44022f24f8b1cb7b7f87d8e9b9206
proyectoA44 <aproyectos44@gmail.com>
2021-11-18 00:01:01 -0600

    cambios

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyBraK2kWOx5dXNAUmH1-JOWN_amX1JquE4",
+  authDomain: "fbautenn.firebaseapp.com",
+  projectId: "fbautenn",
+  storageBucket: "fbautenn.appspot.com",
+  messagingSenderId: "687511025503",
+  appId: "1:687511025503:web:31d62e74bd3ec034dca8ea",
+  measurementId: "G-2GCDLTNLP9"
+    });
